18cfaffe5989e3e3d2ded72f81d109213630840d
Jiaqi Shen <gleiphir2769@gmail.com>
2023-10-29 17:08:43 +0800

    feat: expand Cli args

--- src/cli.rs
+++ src/cli.rs
@@ -2,6 +2,47 @@ use clap::Parser;
 
 #[derive(Parser)]
 pub struct Cli {
-    #[arg(long)]
+    #[clap(short, long)]
     pub stack_size: Option<u64>,
+
+    #[clap(long)]
+    pub version: Option<u64>,
+
+    #[clap(short, long, default_value = "1")]
+    pub uid: u64,
+
+    #[clap(short, long, default_value = "/")]
+    pub mnt: String,
+
+    #[clap(short, long)]
+    pub cmd: Option<String>,
+
+    #[clap(short, long)]
+    pub arg: Option<String>,
+
+    #[clap(short, long, default_value = "false")]
+    pub verbose: bool,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_no_args() {
+        let opts = Cli::parse_from(&["test_program"]);
+        assert_eq!(opts.verbose, false);
+    }
+
+    #[test]
+    fn test_verbose_arg() {
+        let opts = Cli::parse_from(&["test_program", "-v"]);
+        assert_eq!(opts.verbose, true);
+    }
+
+    #[test]
+    fn test_uid_arg() {
+        let opts = Cli::parse_from(&["test_program", "-u=2"]);
+        assert_eq!(opts.uid, 2);
+    }
 }
